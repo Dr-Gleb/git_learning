84d314147509aa4fbcfbd3047f60431fb41f8a3b
Dr-Gleb <glebnefedov@gmail.com>
2022-10-09 15:52:06 +0300

    ass

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Student student = new Student("Gleb", 5.0);
+        Student student1 = new Student("Misha", 5.0);
+
         System.out.println(student);
     }
 }
